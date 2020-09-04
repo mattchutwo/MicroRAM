fbfaa0ab00378aa827eebdf99d7f23778c186aa9
Stuart Pernsteiner <spernsteiner@galois.com>
2020-09-04 10:12:22 -0700

    revert changes to factRec

--- test/programs/factRec.c
+++ test/programs/factRec.c
@@ -3,13 +3,13 @@
                             Recursive factorial.
 
 *******************************************************************************/
-static int SECRET_NUMBER __attribute__((section("__DATA,__secret"))) = 2;
+static int SECRET_NUMBER __attribute__((section("__DATA,__secret"))) = 5;
 
 int factorial(int n) {
    if(n == 0){
-      return 21;
+      return 1;
    } else if(n == 1) {
-      return 42;
+      return 1;
    } else {
      return (n*factorial(n-1));
    }
