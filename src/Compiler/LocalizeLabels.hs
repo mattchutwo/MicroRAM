f39799494c0752975676a152ed0e1b446e263f95
Stuart Pernsteiner <spernsteiner@galois.com>
2020-09-14 10:55:03 -0700

    LocalizeLabels: apply renaming to phi node predecessor labels

--- src/Compiler/LocalizeLabels.hs
+++ src/Compiler/LocalizeLabels.hs
@@ -31,11 +31,15 @@ localizeFunc funcNames func = func { funcBlocks = map goBB $ funcBlocks func }
     goBB (BB name body term dag) =
       BB (rename name) (map goInstr body) (map goInstr term) (map rename dag)
 
-    goInstr (IRI i m) = IRI (fmap goOperand i) m
+    goInstr (IRI i m) = IRI (goRTLInstr i) m
     goInstr (MRI i m) = MRI (fmap goOperand i) m
 
     goOperand (Label s) = Label $ show $ rename $ read s
     goOperand o = o
+
+    goRTLInstr (RPhi ret inps) =
+      RPhi ret (map (\(op, label) -> (goOperand op, rename label)) inps)
+    goRTLInstr i = fmap goOperand i
 
     rename n
       | Set.member n funcNames = n
