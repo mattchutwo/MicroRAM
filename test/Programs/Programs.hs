9a33d7b45e5c0279173d9ce2af898e7fbf1990f9
santiago <scuellar@users.noreply.github.com>
2021-03-11 16:40:46 -0800

    Add memcpy to tests.

--- test/Programs/Programs.hs
+++ test/Programs/Programs.hs
@@ -117,7 +117,7 @@ testLoops = ManyTests "Conditionals, Branching and loops" $ OneTest <$>
     ,testResult = 42
     } : -}
 --  defaultTest { "Hello world" "test/programs/hello.ll" 50 [] 0 :
-  defaultTest {
+    defaultTest {
     testName = "Memcpy"
     ,fileName ="test/Programs/memcpy.ll"
     ,testLen = 300
