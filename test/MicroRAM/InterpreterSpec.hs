757b14399929bd1a13fbf3fd67e392c9e321f30a
santiago <scuellar@users.noreply.github.com>
2021-10-25 12:38:16 -0400

    Fix tests to run with recent changes. Added an Arbitrary instance for ShortByteStrings.

--- test/MicroRAM/InterpreterSpec.hs
+++ test/MicroRAM/InterpreterSpec.hs
@@ -49,7 +49,7 @@ _ppList all =  putStr $ concat $ map (\st -> show st ++ "\n") all
 list2InitMem :: [MWord] -> InitialMem
 list2InitMem ls = map word2InitSeg $ zip [0..] ls 
   where word2InitSeg :: (MWord,MWord) -> InitMemSegment
-        word2InitSeg (loc,val) = InitMemSegment False False False loc 1 (Just [val]) (Just [Vec.replicate wordBytes untainted])
+        word2InitSeg (loc,val) = InitMemSegment "list2Initmem" False False False loc 1 (Just [val]) (Just [Vec.replicate wordBytes untainted])
 
 
 
