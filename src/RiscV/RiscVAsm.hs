543544348b701ef10eb37333396f88fd8a2245ab
santiago <scuellar@users.noreply.github.com>
2022-02-14 14:35:17 -0500

    Fix documentation, make tables look good in Haddock

--- src/RiscV/RiscVAsm.hs
+++ src/RiscV/RiscVAsm.hs
@@ -16,50 +16,37 @@ data Instr
 --}
 
 
-{- | Modifiers
-`%lo(symbol)`
-The low 12 bits of absolute address for symbol.
-
-`%hi(symbol)`
-The high 20 bits of absolute address for symbol. 
-
-`%pcrel_lo(label)`
-The low 12 bits of relative address between pc and symbol. The symbol is related to the high part instruction which is marked by label.
-
-`%pcrel_hi(symbol)`
-The high 20 bits of relative address between pc and symbol.
-
-`%got_pcrel_hi(symbol)`
-The high 20 bits of relative address between pc and the GOT entry of symbol.
-
-`%tprel_add(symbol)`
-This is used purely to associate the R_RISCV_TPREL_ADD relocation for TLS relaxation. This one is only valid as the fourth operand to the normally 3 operand add instruction.
-
-`%tprel_lo(symbol)`
-The low 12 bits of relative address between tp and symbol.
-
-`%tprel_hi(symbol)`
-The high 20 bits of relative address between tp and symbol.
-
-`%tls_ie_pcrel_hi(symbol)`
-The high 20 bits of relative address between pc and GOT entry.
-
-`%tls_gd_pcrel_hi(symbol)`
-The high 20 bits of relative address between pc and GOT entry. 
-
+{- | Modifiers: The RISC-V assembler supports following modifiers for relocatable addresses used in RISC-V instruction operands. These expressions should be resolved during linking.
 -}
-
-data Modifier
-  = ModLo
-  | ModHi
-  | ModPcrel_lo
-  | ModPcrel_hi
-  | ModGot_pcrel_hi
-  | ModTprel_add
-  | ModTprel_lo
-  | ModTprel_hi
-  | ModTls_ie_pcrel_hi
-  | ModTls_gd_pcrel_hi
+data Modifier =
+  ModLo                -- ^ @%lo(symbol)@ The low 12 bits of absolute
+                       -- address for symbol.
+  | ModHi              -- ^ @%hi(symbol)@ The high 20 bits of absolute
+                       -- address for symbol.
+  | ModPcrel_lo        -- ^ @%pcrel_lo(label)@ The low 12 bits of
+                       -- relative address between pc and symbol. The
+                       -- symbol is related to the high part instruction
+                       -- which is marked by label.
+  | ModPcrel_hi        -- ^ @%pcrel_hi(symbol)@ The high 20 bits of
+                       -- relative address between pc and symbol.
+  | ModGot_pcrel_hi    -- ^ @%got_pcrel_hi(symbol)@ The high 20 bits of
+                       -- relative address between pc and the GOT entry
+                       -- of symbol.
+  | ModTprel_add       -- ^ @%tprel_add(symbol)@ This is used purely to
+                       -- associate the R_RISCV_TPREL_ADD relocation for
+                       -- TLS relaxation. This one is only valid as the
+                       -- fourth operand to the normally 3 operand add
+                       -- instruction.
+  | ModTprel_lo        -- ^ @%tprel_lo(symbol)@ The low 12 bits of
+                       -- relative address between tp and symbol.
+  | ModTprel_hi        -- ^ @%tprel_hi(symbol)@ The high 20 bits of
+                       -- relative address between tp and symbol.
+  | ModTls_ie_pcrel_hi -- ^ @%tls_ie_pcrel_hi(symbol)@ The high 20
+                       -- bits of relative address between pc and GOT
+                       -- entry.
+  | ModTls_gd_pcrel_hi -- ^ @%tls_gd_pcrel_hi(symbol)@ The high 20
+                       -- bits of relative address between pc and GOT
+                       -- entry.
   deriving (Show, Eq, Ord)
 
 instance Arbitrary Modifier where
@@ -76,7 +63,7 @@ instance Arbitrary Modifier where
       , ModTls_ie_pcrel_hi
       , ModTls_gd_pcrel_hi]
 
-{-- RiscV Assembly accepts arbitrary operations on immediates, to be resolved at link time. Normally these expressions are small with, at most, a modifier, a binary operation a symbola and a constant: e.g. `%hi(__iob+8)`
+{-- RiscV Assembly accepts arbitrary operations on immediates, to be resolved at link time. Normally these expressions are small with, at most, a modifier, a binary operation a symbola and a constant: e.g. @%hi(__iob+8)@
 
 --}
 data ImmOp =
@@ -113,21 +100,33 @@ instance Arbitrary Imm where
     , ImmBinOp   <$> arbitrary <*> arbitrary <*> arbitrary ]
   
 {-- $regs
-Registers: 
+Registers:
+
 +----------+------------------------------------+--------+
 | Register |            Description             | Saver  |
-+----------+------------------------------------+--------+
++==========+====================================+========+
 | x0       | hardwired zero                     | -      |
++----------+------------------------------------+--------+
 | x1       | return address                     | Caller |
++----------+------------------------------------+--------+
 | x2       | stack pointer                      | Callee |
++----------+------------------------------------+--------+
 | x3       | global pointer                     | -      |
++----------+------------------------------------+--------+
 | x4       | thread pointer                     | -      |
++----------+------------------------------------+--------+
 | x5-7     | temporary registers                | Caller |
++----------+------------------------------------+--------+
 | x8       | saved register / frame pointer     | Callee |
++----------+------------------------------------+--------+
 | x9       | saved register                     | Callee |
++----------+------------------------------------+--------+
 | x10-11   | function arguments / return values | Caller |
++----------+------------------------------------+--------+
 | x12-17   | function arguments                 | Caller |
++----------+------------------------------------+--------+
 | x18-27   | saved registers                    | Callee |
++----------+------------------------------------+--------+
 | x28-31   | temporary registers                | Caller |
 +----------+------------------------------------+--------+
 --}
@@ -181,12 +180,17 @@ instance Arbitrary Reg where
 
 +---------------------+---------------------------+------------------------------------+
 |       Format        |           Name            |             Pseudocode             |
-+---------------------+---------------------------+------------------------------------+
++=====================+===========================+====================================+
 | BEQ rs1,rs2,offset  | Branch Equal              | if rs1 = rs2 then pc ← pc + offset |
++---------------------+---------------------------+------------------------------------+
 | BNE rs1,rs2,offset  | Branch Not Equal          | if rs1 ≠ rs2 then pc ← pc + offset |
++---------------------+---------------------------+------------------------------------+
 | BLT rs1,rs2,offset  | Branch Less Than          | if rs1 < rs2 then pc ← pc + offset |
++---------------------+---------------------------+------------------------------------+
 | BGE rs1,rs2,offset  | Branch Greater than Equal | if rs1 ≥ rs2 then pc ← pc + offset |
++---------------------+---------------------------+------------------------------------+
 | BLTU rs1,rs2,offset | Branch Less Than Unsigned | if rs1 < rs2 then pc ← pc + offset |
++---------------------+---------------------------+------------------------------------+
 | BGEU rs1,rs2,offset | Branch Greater than Equal | if rs1 ≥ rs2 then pc ← pc + offset |
 |                     | Unsigned                  |                                    |
 +---------------------+---------------------------+------------------------------------+
@@ -215,51 +219,89 @@ instance Arbitrary BranchCond where
 
 +---------------------+-----------------------+------------------------------+
 |       Format        |         Name          |          Pseudocode          |
-+---------------------+-----------------------+------------------------------+
++=====================+=======================+==============================+
 | LUI rd,imm          | Load Upper Immediate  | rd ← imm                     |
++---------------------+-----------------------+------------------------------+
 | AUIPC rd,offset     | Add Upper Imm. to PC  | rd ← pc + offset             |
++---------------------+-----------------------+------------------------------+
 | JAL rd,offset       | Jump and Link         | "rd ← pc + length(inst)      |
 | .                   | .                     | pc ← pc + offset"            |
++---------------------+-----------------------+------------------------------+
 | JALR rd,rs1,offset  | Jump and Link Reg     | "rd ← pc + length(inst)      |
 | .                   | .                     | pc ← (rs1 + offset) ∧ -2"    |
++---------------------+-----------------------+------------------------------+
 | BEQ rs1,rs2,off     | Branch Equal          | if rs1 = rs2 : pc ← pc + off |
++---------------------+-----------------------+------------------------------+
 | BNE rs1,rs2,off     | Branch Not Equal      | if rs1 ≠ rs2 : pc ← pc + off |
++---------------------+-----------------------+------------------------------+
 | BLT rs1,rs2,off     | Branch Less Than      | if rs1 < rs2 : pc ← pc + off |
++---------------------+-----------------------+------------------------------+
 | BGE rs1,rs2,off     | Branch Greater        | if rs1 ≥ rs2 : pc ← pc + off |
 | .                   | than or Equal         |                              |
++---------------------+-----------------------+------------------------------+
 | BLTU rs1,rs2,off    | Branch Less Than      | if rs1 < rs2 : pc ← pc + off |
 | .                   | Unsigned              |                              |
++---------------------+-----------------------+------------------------------+
 | BGEU rs1,rs2,offset | Branch Greater than   | if rs1 ≥ rs2 : pc ← pc + off |
 | .                   | or Equal Unsigned     |                              |
++---------------------+-----------------------+------------------------------+
 | LB rd,offset(rs1)   | Load Byte             | rd ← s8[rs1 + offset]        |
++---------------------+-----------------------+------------------------------+
 | LH rd,offset(rs1)   | Load Half             | rd ← s16[rs1 + offset]       |
++---------------------+-----------------------+------------------------------+
 | LW rd,offset(rs1)   | Load Word             | rd ← s32[rs1 + offset]       |
++---------------------+-----------------------+------------------------------+
 | LBU rd,offset(rs1)  | Load Byte Unsigned    | rd ← u8[rs1 + offset]        |
++---------------------+-----------------------+------------------------------+
 | LHU rd,offset(rs1)  | Load Half Unsigned    | rd ← u16[rs1 + offset]       |
++---------------------+-----------------------+------------------------------+
 | SB rs2,offset(rs1)  | Store Byte            | u8[rs1 + offset] ← rs2       |
++---------------------+-----------------------+------------------------------+
 | SH rs2,offset(rs1)  | Store Half            | u16[rs1 + offset] ← rs2      |
++---------------------+-----------------------+------------------------------+
 | SW rs2,offset(rs1)  | Store Word            | u32[rs1 + offset] ← rs2      |
++---------------------+-----------------------+------------------------------+
 | ADDI rd,rs1,imm     | Add Immediate         | rd ← rs1 + sx(imm)           |
++---------------------+-----------------------+------------------------------+
 | SLTI rd,rs1,imm     | Set Less Than Imm.    | rd ← sx(rs1) < sx(imm)       |
++---------------------+-----------------------+------------------------------+
 | SLTIU rd,rs1,imm    | Set Less Than Imm.    | rd ← ux(rs1) < ux(imm)       |
 | .                   | Unsigned              |                              |
++---------------------+-----------------------+------------------------------+
 | XORI rd,rs1,imm     | Xor Immediate         | rd ← ux(rs1) ⊕ ux(imm)       |
++---------------------+-----------------------+------------------------------+
 | ORI rd,rs1,imm      | Or Immediate          | rd ← ux(rs1) ∨ ux(imm)       |
++---------------------+-----------------------+------------------------------+
 | ANDI rd,rs1,imm     | And Immediate         | rd ← ux(rs1) ∧ ux(imm)       |
++---------------------+-----------------------+------------------------------+
 | SLLI rd,rs1,imm     | Shift Left Log. Imm   | rd ← ux(rs1) « ux(imm)       |
++---------------------+-----------------------+------------------------------+
 | SRLI rd,rs1,imm     | Shift Right Log. Imm  | rd ← ux(rs1) » ux(imm)       |
++---------------------+-----------------------+------------------------------+
 | SRAI rd,rs1,imm     | Shift Right Arith Imm | rd ← sx(rs1) » ux(imm)       |
++---------------------+-----------------------+------------------------------+
 | ADD rd,rs1,rs2      | Add                   | rd ← sx(rs1) + sx(rs2)       |
++---------------------+-----------------------+------------------------------+
 | SUB rd,rs1,rs2      | Subtract              | rd ← sx(rs1) - sx(rs2)       |
++---------------------+-----------------------+------------------------------+
 | SLL rd,rs1,rs2      | Shift Left Logical    | rd ← ux(rs1) « rs2           |
++---------------------+-----------------------+------------------------------+
 | SLT rd,rs1,rs2      | Set Less Than         | rd ← sx(rs1) < sx(rs2)       |
++---------------------+-----------------------+------------------------------+
 | SLTU rd,rs1,rs2     | Set Less Than Unsig.  | rd ← ux(rs1) < ux(rs2)       |
++---------------------+-----------------------+------------------------------+
 | XOR rd,rs1,rs2      | Xor                   | rd ← ux(rs1) ⊕ ux(rs2)       |
++---------------------+-----------------------+------------------------------+
 | SRL rd,rs1,rs2      | Shift Right Logical   | rd ← ux(rs1) » rs2           |
++---------------------+-----------------------+------------------------------+
 | SRA rd,rs1,rs2      | Shift Right Arith.    | rd ← sx(rs1) » rs2           |
++---------------------+-----------------------+------------------------------+
 | OR rd,rs1,rs2       | Or                    | rd ← ux(rs1) ∨ ux(rs2)       |
++---------------------+-----------------------+------------------------------+
 | AND rd,rs1,rs2      | And                   | rd ← ux(rs1) ∧ ux(rs2)       |
++---------------------+-----------------------+------------------------------+
 | FENCE pred,succ     | Fence                 |                              |
++---------------------+-----------------------+------------------------------+
 | FENCE.I             | Fence Instruction     |                              |
 +---------------------+-----------------------+------------------------------+
 
